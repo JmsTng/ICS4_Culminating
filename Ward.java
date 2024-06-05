eb56d83ce0f605f9c01de1d00555b8e3123dc49e
Lee <Hayden.Lee2@student.tdsb.on.ca>
2024-06-05 15:10:25 -0400

    setup ward class

---
+++ Ward.java
@@ -0,0 +1,35 @@
+public class Ward{
+   private int matchSymptomReq;
+   private int maxEquipment;
+   private int numEquipment;
+   
+   public Ward(int matchSymptomReq, int maxEquipment){
+      this.matchSymptomReq = matchSymptomReq;
+      this.maxEquipment = maxEquipment;
+      this. numEquipment = 0;
+   }
+   
+   public double getOperatingCost(){
+   
+   }
+   
+   public boolean addPatient(Patient p){
+   
+   }
+   
+   public boolean removePatient(Patient p){
+   
+   }
+   
+   public boolean addEquipment(Equipment e, int num){
+   
+   }
+   
+   public boolean removeEquipment(Equipment e, int num){
+   
+   }
+   
+   public boolean determineSuitability(Patient p){
+   
+   }
+}
