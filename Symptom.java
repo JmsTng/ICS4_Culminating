dd4ffec19332bc7f8cbc6b6530a65d76fa203d76
James Tung <james.tung@student.tdsb.on.ca>
2024-06-10 13:54:59 -0400

    MedicalHistory, Patient worked on need toString on Symptom

--- Symptom.java
+++ Symptom.java
@@ -2,7 +2,11 @@ public abstract class Symptom {
     private String name;
     private String description;
     
-    
+
+    public Symptom(String name) {
+        this.name = name;
+    }
+
     public Symptom(String name, String description) {
         this.name = name;
         this.description = description;
