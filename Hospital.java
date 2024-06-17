58dd31bb4061c7831a34974f8a85681cb1a93c6e
gqbr1 <gabriel.shu1@hotmail.com>
2024-06-16 22:44:46 -0400

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -4,6 +4,8 @@ import Ward.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.lang.Exception;
 
 public class Hospital {
 
@@ -31,7 +33,7 @@ public class Hospital {
       this.numPatients = 0;
       this.numStaff = 0;
       
-      this.file = "staff.txt"
+      this.file = "staff.txt";
       this.storage = new Storage();
       this.sc = new Scanner(System.in);
    }
@@ -43,28 +45,32 @@ public class Hospital {
       //Prompts user for type of ward
       System.out.println("Select ward type to add:\n(1) ICU\n(2) Oncology\n(3) Emergency\n(4) Paedology\n(5) Other");
       //Switch case :(
-      switch (sc.nextInt()) {
-         case 1 -> {
-            wards.add(new ICU());
+      try{
+         switch (sc.nextInt()) {
+            case 1 -> {
+               wards.add(new ICU());
+            }
+            case 2 -> {
+               wards.add(new Oncology());
+            }
+            case 3 -> {
+               wards.add(new Emergency());
+            }
+            case 4 -> {
+               wards.add(new Paedology());
+            }
+            case 5 -> {
+               wards.add(new Ward());
+            }
+            default -> {
+               return false;
+            }
          }
-         case 2 -> {
-            wards.add(new Oncology());
-         }
-         case 3 -> {
-            wards.add(new Emergency());
-         }
-         case 4 -> {
-            wards.add(new Paedology());
-         }
-         case 5 -> {
-            wards.add(new Ward());
-         }
-         default -> {
-            return false;
-         }
+         numWards++;
+         return true;
+      }catch (Exception e){
+         return false;
       }
-      numWards++;
-      return true;
    }
    
    //Removes ward from ward array list
@@ -85,28 +91,32 @@ public class Hospital {
    public boolean addStaff(String name, double salary, double overtime) {
       //Prompt user for type of staff
       System.out.println("Select staff to add:\n(1) Doctor\n(2) Medical\n(3) Admin\n(4) Other");
-      switch (sc.nextInt()) {
-         case 1 -> {
-            //Prompts for specialty if doctor is selected
-            System.out.println("Enter the doctor's specialty:\n");
-            String specialty = sc.nextLine();
-            staff.add(new Doctor(specialty, name, salary, overtime));
+      try{
+         switch (sc.nextInt()) {
+            case 1 -> {
+               //Prompts for specialty if doctor is selected
+               System.out.println("Enter the doctor's specialty:\n");
+               String specialty = sc.nextLine();
+               staff.add(new Doctor(specialty, name, salary, overtime));
+            }
+            case 2 -> {
+               staff.add(new Medical(name, salary, overtime));
+            }
+            case 3 -> {
+               staff.add(new Admin(name, salary, overtime));
+            }
+            case 4 -> {
+               staff.add(new Staff(name, salary, overtime));
+            }
+            default -> {
+               return false;
+            }
          }
-         case 2 -> {
-            staff.add(new Medical(name, salary, overtime));
-         }
-         case 3 -> {
-            staff.add(new Admin(name, salary, overtime));
-         }
-         case 4 -> {
-            staff.add(new Staff(name, salary, overtime));
-         }
-         default -> {
-            return false;
-         }
+         numStaff++;
+         return true;
+      }catch (Exception e){
+         return false;
       }
-      numStaff++;
-      return true;
    }
    
    //Removes staff from staff array list
@@ -127,22 +137,26 @@ public class Hospital {
    public boolean assignEquipment() {
       //Prompts user for add or remove
       System.out.println("Select an option:\n(1) Add equipment\n(2) Remove equipment");
-      int select = sc.nextInt();
-      if (select <= 2 && select >= 1){
-         //Prompts user for type of equipment and amount
-         System.out.println("Enter an equipment:");
-         String type = sc.nextLine();
-         System.out.println("Enter an amount:");
-         int amount = sc.nextInt();
-         //Runs wards methods for adding or removing equipment
-         if (select == 1){
-            storage.addEquipment(type, amount);
+      try{
+         int select = sc.nextInt();
+         if (select <= 2 && select >= 1){
+            //Prompts user for type of equipment and amount
+            System.out.println("Enter an equipment:");
+            String type = sc.nextLine();
+            System.out.println("Enter an amount:");
+            int amount = sc.nextInt();
+            //Runs wards methods for adding or removing equipment
+            if (select == 1){
+               storage.addEquipment(type, amount);
+            }else{
+               storage.removeEquipment(type, amount);
+            }
+            return true;
          }else{
-            storage.removeEquipment(type, amount);
+            return false;
          }
-         return true;
-      }else{
-         return false
+      }catch (Exception e){
+         return false;
       }
    }
    
@@ -151,24 +165,28 @@ public class Hospital {
    public boolean addPatient(String name) {
       //Prompts user for type of patient
       System.out.println("Select patient type to add:\n(1) Normal\n(2) Child");
-      switch (sc.nextInt()) {
-         case 1 -> {
-            patients.add(new Patient(name));
+      try{
+         switch (sc.nextInt()) {
+            case 1 -> {
+               patients.add(new Patient(name));
+            }
+            case 2 -> {
+               patients.add(new Child(name));
+            }
+            default -> {
+               return false;
+            }
          }
-         case 2 -> {
-            patients.add(new Child(name));
-         }
-         default -> {
-            return false;
-         }
+         numPatients++;
+         return true;
+      }catch (Exception e){
+         return false;
       }
-      numPatients++;
-      return true;
    }
    
    //Removes patient from patient array list
    //Returns true if successful, otherwise returns false
-   public void removePatient(String id) {
+   public boolean removePatient(String id) {
       //Checks if any patients have a matching id
       for (int i = 0; i < patients.size(); i++){
          if (patients.get(i).getId().equals(id)){
@@ -194,19 +212,23 @@ public class Hospital {
    public ArrayList<Patient> searchPatient() {
       //Prompts user for field to search by
       System.out.println("Select field to search by:\n(1) Name\n(2) ID");
-      int searchType = sc.nextInt();
-      if (searchType >= 1 && searchType <= 2){
-         System.out.print("Enter search query: ");
-         String search = sc.nextLine();
-         //Creates array list of patients and searches through entire patient list for matches
-         ArrayList<Patient> patientList = new ArrayList<Patient>();
-         for (int i = 0; i < numPatients; i++){
-            if (pSearch(searchType, search, i)){
-               patientList.add(patients[i]);
+      try{
+         int searchType = sc.nextInt();
+         if (searchType >= 1 && searchType <= 2){
+            System.out.print("Enter search query: ");
+            String search = sc.nextLine();
+            //Creates array list of patients and searches through entire patient list for matches
+            ArrayList<Patient> patientList = new ArrayList<Patient>();
+            for (int i = 0; i < numPatients; i++){
+               if (pSearch(searchType, search, i)){
+                  patientList.add(patients.get(i));
+               }
             }
+            return patientList;
+         }else{
+            return null;
          }
-         return patientList;
-      }else{
+      }catch (Exception e){
          return null;
       }
    }
@@ -225,19 +247,23 @@ public class Hospital {
    public ArrayList<Staff> searchStaff() {
       //Prompts user for field to search by
       System.out.println("Select field to search by:\n(1) Name\n(2) Employee Number");
-      int searchType = sc.nextInt();
-      if (searchType >= 1 && searchType <= 2){
-         System.out.print("Enter search query: ");
-         String search = sc.nextLine();
-         //Creates array list of staff and searches through entire staff list for matches
-         ArrayList<Staff> staffList = new ArrayList<Staff>();
-         for (int i = 0; i < numStaff; i++){
-            if (sSearch(searchType, search, i)){
-               staffList.add(staff[i]);
+      try{
+         int searchType = sc.nextInt();
+         if (searchType >= 1 && searchType <= 2){
+            System.out.print("Enter search query: ");
+            String search = sc.nextLine();
+            //Creates array list of staff and searches through entire staff list for matches
+            ArrayList<Staff> staffList = new ArrayList<Staff>();
+            for (int i = 0; i < numStaff; i++){
+               if (sSearch(searchType, search, i)){
+                  staffList.add(staff.get(i));
+               }
             }
+            return staffList;
+         }else{
+            return null;
          }
-         return staffList;
-      }else{
+      }catch (Exception e){
          return null;
       }
    }
@@ -256,30 +282,34 @@ public class Hospital {
    public Patient[] sortPatient() {
       //Prompts user for field to sort by
       System.out.println("Select field to sort by:\n(1) Name\n(2) Triage");
-      int type = sc.nextInt();
-      if (type >= 1 && type <= 2){
-         //Creates new patient array and adds all patients
-         Patient[] patientList = new Patient[numPatients];
-         for (int i = 0; i < numPatients; i++){
-            patientList[i] = patients.get(i);
-         }
-         boolean sorted = false;
-         //Sorts patients using bubble sort
-         for (int i = 0; i < numPatients && sorted == false; i++){
-            int index = 0;
-            sorted = true;
-            for (int j = 1; j < numPatients-i; j++){
-               if (pSort(type, index, j) < 0){
-                  Patient p = patientList[j];
-                  patientList[j] = patientList[index];
-                  patientList[index] = p;
-                  index = j;
-                  sorted = false;
+      try{
+         int type = sc.nextInt();
+         if (type >= 1 && type <= 2){
+            //Creates new patient array and adds all patients
+            Patient[] patientList = new Patient[numPatients];
+            for (int i = 0; i < numPatients; i++){
+               patientList[i] = patients.get(i);
+            }
+            boolean sorted = false;
+            //Sorts patients using bubble sort
+            for (int i = 0; i < numPatients && sorted == false; i++){
+               int index = 0;
+               sorted = true;
+               for (int j = 1; j < numPatients-i; j++){
+                  if (pSort(type, index, j) < 0){
+                     Patient p = patientList[j];
+                     patientList[j] = patientList[index];
+                     patientList[index] = p;
+                     index = j;
+                     sorted = false;
+                  }
                }
             }
+            return patientList;
+         }else{
+            return null;
          }
-         return patientList;
-      }else{
+      }catch (Exception e){
          return null;
       }
    }
@@ -298,30 +328,34 @@ public class Hospital {
    public Staff[] sortStaff() {
       //Prompts user for field to sort by
       System.out.println("Select field to sort by:\n(1) Name\n(2) Salary");
-      int type = sc.nextInt();
-      if (type >= 1 && type <= 2){
-         //Creates new staff array and adds all staff
-         Staff[] staffList = new Staff[numStaff];
-         for (int i = 0; i < numPatients; i++){
-            staffList[i] = staff.get(i);
-         }
-         boolean sorted = false;
-         //Sorts staff using bubble sort
-         for (int i = 0; i < numStaff && sorted == false; i++){
-            int index = 0;
-            sorted = true;
-            for (int j = 1; j < numPatients-i; j++){
-               if (sSort(type, index, j) < 0){
-                  Staff s = staffList[j];
-                  staffList[j] = staffList[index];
-                  staffList[index] = s;
-                  index = j;
-                  sorted = false;
+      try{
+         int type = sc.nextInt();
+         if (type >= 1 && type <= 2){
+            //Creates new staff array and adds all staff
+            Staff[] staffList = new Staff[numStaff];
+            for (int i = 0; i < numStaff; i++){
+               staffList[i] = staff.get(i);
+            }
+            boolean sorted = false;
+            //Sorts staff using bubble sort
+            for (int i = 0; i < numStaff && sorted == false; i++){
+               int index = 0;
+               sorted = true;
+               for (int j = 1; j < numStaff - i; j++){
+                  if (sSort(type, index, j) < 0){
+                     Staff s = staffList[j];
+                     staffList[j] = staffList[index];
+                     staffList[index] = s;
+                     index = j;
+                     sorted = false;
+                  }
                }
             }
+            return staffList;
+         }else{
+            return null;
          }
-         return staffList;
-      }else{
+      }catch (Exception e){
          return null;
       }
    }
@@ -345,8 +379,8 @@ public class Hospital {
       for (int i = 0; i < numStaff; i++){
          income -= staff.get(i).getMonthlySalary();
       }
-      for (int i = 0; i < numPatient; i++){
-         income += patient.get(i).bill();
+      for (int i = 0; i < numPatients; i++){
+         income += patients.get(i).bill();
       }
       return income;
    }
@@ -368,6 +402,7 @@ public class Hospital {
             }
             write.write(temp.getName() + "\n" + temp.getEmployeeNum + "\n" + temp.getSalary() + "\n" + temp.getOvertimeSalary());
          }
+         write.close();
       }catch (IOException e){
       }
    }
